aa0763c59ed1a9e9c5a082a3724be5fe56444bfd
Smmmhhh <kua0408@gmail.com>
2024-01-22 13:01:21 +0900

    [ADD] 20240122 폰켓몬

---
+++ Algorithm/src/폰켓몬.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class 폰켓몬 {
+
+	public static void main(String[] args) {
+		
+		int[] nums = {3,1,2,3};
+		System.out.println(solution(nums));
+
+	}
+	
+    public static int solution(int[] nums) {
+        int answer = 0;
+        int count = 0;
+        Set<Integer> hs = new HashSet<>();
+        
+        // HashSet 자료구조에 nums 배열의 값 add
+        for(int i = 0; i < nums.length; i++) {
+        	hs.add(nums[i]);
+        }
+        
+        // N / 2 만큼 폰켓몬을 선택
+        count = nums.length / 2;
+        
+        // 폰켓몬 수 구하기
+        for(int i = 1; i <= count; i++) {
+        	if(i <= hs.size()) answer++;
+        }
+       
+        return answer;
+    }
+
+}
